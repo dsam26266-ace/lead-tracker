a59e2cfef57d503a57fb571e887e0c8500c7955f
Sankalpa Das <dsam26266@gmail.com>
2026-07-10 18:57:02 +0530

    innerHtmland index bug fix

--- index.js
+++ index.js
@@ -2,9 +2,16 @@
 const save_btn_el = document.getElementById("input-btn")
 
 const input_el = document.getElementById("input-el");
-let leads = []
+let leads = ["hseieddij","dhiedeopfhi"]
+
+const ul_el = document.getElementById("unorderlist-el")
 
 save_btn_el.addEventListener("click",function(){
     leads.push(input_el.value)
     console.log(leads)
 })
+
+
+for(let i=0;i<=leads.length;i++){
+    ul_el.innerHTML += "<li>"+leads[i]+"</li>"
+}
