0d545aee37bac8a3ed1606d8ade95fc653a1fedd
Sankalpa Das <dsam26266@gmail.com>
2026-07-11 07:40:12 +0530

    added the saveinput and diaplay the inputs as anchor

--- index.js
+++ index.js
@@ -2,16 +2,30 @@
 const save_btn_el = document.getElementById("input-btn")
 
 const input_el = document.getElementById("input-el");
-let leads = ["hseieddij","dhiedeopfhi"]
+let leads = []
 
 const ul_el = document.getElementById("unorderlist-el")
 
 save_btn_el.addEventListener("click",function(){
     leads.push(input_el.value)
-    console.log(leads)
+    renderLeads()
+    input_el.value = ""
 })
 
+function renderLeads(){
+    let listItems = ""
 
-for(let i=0;i<=leads.length;i++){
-    ul_el.innerHTML += "<li>"+leads[i]+"</li>"
+    for(let i=0;i<leads.length;i++){
+        // listItems += "<li><a href='"+leads[i]+"' target='_blank'>"+leads[i]+"</a></li>"
+        listItems+=`
+            <li>
+                <a href='${leads[i]}' target='_blank'>
+                    ${leads[i]}
+                </a>
+            </li>
+        
+        `
+    }
+
+    ul_el.innerHTML = listItems
 }
